89c32b36c2a453cf1aad6ef66f2043290a44e7ca
sic <arabiran@prans.com>
2024-10-07 16:36:42 +0800

    readjust footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,31 +4,49 @@ import {
   LocationOn,
   PhoneAndroid,
 } from "@mui/icons-material";
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 
 export default function Footer() {
   return (
-    <Box sx={{ padding: "30px" }} id="Contacts">
-      <Box
+    <Box sx={{ padding: "20px" }} id="Contacts">
+      <Stack
         sx={{
           bgcolor: "#fff",
           width: 1,
-          display: "flex",
-          gap: "40px",
-          justifyContent: "center",
-          margin: "16px 0",
         }}
+        direction={{ xs: "column", sm: "column", md: "row" }}
+        spacing={{ xs: 1, sm: 1, md: 1 }}
       >
-        <Box sx={{ display: "flex" }}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: {
+              xs: "center",
+              sm: "center",
+              md: "flex-end",
+            },
+            width: "100%",
+          }}
+        >
           <PhoneAndroid />
           <Typography>+639165831311</Typography>
         </Box>
 
-        <Box sx={{ display: "flex" }}>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: {
+              xs: "center",
+              sm: "center",
+              md: "flex-start",
+            },
+            width: "100%",
+          }}
+        >
           <LocationOn />
-          <Typography>Sa Sulok Fernandez, Taguig</Typography>
+          <Typography noWrap>Sa Sulok Fernandez, Taguig</Typography>
         </Box>
-      </Box>
+      </Stack>
       <Box
         sx={{
           bgcolor: "#fff",
